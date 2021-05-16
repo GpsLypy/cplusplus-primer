32f0478bdb9c85ebc2332246ed0df9217ab2efec
root <root@localhost.localdomain>
2021-05-16 22:31:19 +0800

    first commit

---
+++ test40.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************
+> File Name:     test40.cpp
+> Author:        想名字多费事
+> 微信公众号:    xxxxxxx
+> Created Time:  2021年05月16日 星期日 20时38分48秒
+> Description:   
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+using namespace std;
+
+string aaa;
+int aa;
+int main(){
+    int local1;
+    string local;
+    cout<<aaa<<aa<<local1<<local<<endl;
+}
---
+++ test45.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************
+> File Name:     test45.cpp
+> Author:        想名字多费事
+> 微信公众号:    xxxxxxx
+> Created Time:  2021年05月16日 星期日 22时20分47秒
+> Description:   
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+using namespace std;
+int main(){
+    int i=100,sum=0;
+    for(int i=0;i!=10;i++){
+        sum+=i;
+        cout<<i<<" "<<sum<<endl;
+    }
+}
